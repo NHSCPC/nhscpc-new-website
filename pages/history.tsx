6e0e069d9a372d501a8cb27d2e2dec966c4206b6
Forixaim <thebest20067@gmail.com>
2022-07-13 23:48:12 -0700

    Fix escape errors by switch ' with &apos; and " with &quot;

--- pages/history.tsx
+++ pages/history.tsx
@@ -24,13 +24,13 @@ const History: NextPage = () => {
             <article className="my-16">
                 <h1 className="mx-8 text-center font-semibold text-3xl">Website History</h1>
             <p className="mx-8 text-2xl indent-16">
-                The website was developed by Matthew "Forixaim" Chai, who wanted to dabble a little into web development. As it turned out, he was interested in Next.js so he decided to develop his personal website from it. After his personal website was created, he decided to revamp his club's website and this website was the final product.
+                The website was developed by Matthew &quot;Forixaim&quot; Chai, who wanted to dabble a little into web development. As it turned out, he was interested in Next.js so he decided to develop his personal website from it. After his personal website was created, he decided to revamp his club&apos;s website and this website was the final product.
             </p>
             </article>
 			<article>
                 <h1 className="mx-8 text-center font-semibold text-3xl">Club History</h1>
                 <p className="mx-8 text-2xl indent-16">
-                    Matthew will let others deal with the club history since Matthew don't know much about the history of the club.
+                    Matthew will let others deal with the club history since Matthew don&apos;t know much about the history of the club.
                 </p>
             </article>
 		</main>
