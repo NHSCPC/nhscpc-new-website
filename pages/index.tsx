19ecd9c7dc6fa9db909b983793ddd3a9f12aefb7
Forixaim <thebest20067@gmail.com>
2022-08-24 13:03:12 -0700

    fun

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
 			<div className="flex bg-red-800 mx-0 mb-4 w-screen items-center justify-center h-96 border-b-black shadow-xl">
 				<Image src={logo} width={900} alt="Newport Programming Club Logo"/>
 			</div>
-			<p className="text-2xl font-semibold text-center my-8">Welcome to Newport High School Programming Club! Here we help up and coming students who are interested in programming explore new things and hone their skills via programming competitions.</p>
+			<p className="text-2xl font-semibold text-center my-8">Welcome to Newport High School Programming Club! Here we help up and coming students who are interested in programming explore new things and hone their skills via competitions, projects, and much more!</p>
 		</main>
 	</div>
   )
